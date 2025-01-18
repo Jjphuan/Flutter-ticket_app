0b5996e9c4c3a58f9b2d250c757ba4519384aa6e
workPhuan <workgitphuan@gmail.com>
2025-01-18 14:25:12 +0800

    update search view

--- lib/base/res/media.dart
+++ lib/base/res/media.dart
@@ -2,4 +2,5 @@ class AppMedia {
   static const _image_path = "assets/images";
 
   static const logo = '$_image_path/Travel.png';
+  static const hotel_view = '$_image_path/hotel_view.jpg';
 }
--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/Style/app_style.dart';
+import 'package:ticket_app/base/res/media.dart';
 import 'package:ticket_app/base/widgets/app_double_text.dart';
 import 'package:ticket_app/screens/search/widgets/search_button.dart';
 import 'package:ticket_app/screens/search/widgets/search_field.dart';
@@ -10,6 +11,8 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: AppStyle.bgColor,
       body: ListView(
@@ -34,6 +37,101 @@ class SearchScreen extends StatelessWidget {
             smallText: "View All", 
             function: () => Navigator.pushNamed(context,"all_tickets")
             ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: 410,
+                    width: screenSize.width*0.44,
+                    padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 3,
+                          spreadRadius: 1
+                        )
+                        ]
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 190,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            image: const DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage(AppMedia.hotel_view)
+                            )
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        Text(
+                          "20% discount on the early booking for this flight. Let Check it!",
+                          style: AppStyle.headTextSmall,
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 14,vertical:10),
+                        width: screenSize.width*0.44,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: const Color.fromARGB(255, 115, 189, 221),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Discount a\nsurvey",
+                              style: AppStyle.headTextSmall.copyWith(color: Colors.white)
+                            ),
+                            const SizedBox(height: 15),
+                            Text(
+                              "Take a survey about our services and get discount.",
+                              style: AppStyle.headTextSmaller
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height:10),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 14,vertical:10),
+                        width: screenSize.width*0.44,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: const Color.fromARGB(255, 230, 141, 34),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Help us improve",
+                              style: AppStyle.headTextSmall.copyWith(color: Colors.white)
+                            ),
+                            const SizedBox(height: 15),
+                            Text(
+                              "Give us suggestion to improve your experience",
+                              style: AppStyle.headTextSmaller
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+            ],
+          )
         ],
       ),
     );
